20d5e6262088970d1ed6eacbd8a0445700424617
Karolina Gorlewska <k.gorlewska@gmail.com>
2019-02-23 20:22:36 +0000

    Add player move

--- js/script.js
+++ js/script.js
@@ -8,9 +8,10 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+// Computer move
+
 var computerMove, randomNumber;
 
-// Computer move
 
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log(('wylosowana liczba to: ' + randomNumber));
@@ -22,3 +23,20 @@ if (randomNumber == '1') {
   computerMove = 'nożyce';
 }
 printMessage(('Mój ruch: ' + computerMove));
+
+// Player move
+
+var playerMove, playerInput;
+
+playerInput = window.prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log(('Wpisana odpowiedź to: ' + playerInput));
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'kamień';
+}
+printMessage(('Twój ruch: ' + playerMove));
