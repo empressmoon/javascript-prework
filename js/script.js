ccdafef35b9f83bce09634412447585a432e2c07
Karolina Gorlewska <k.gorlewska@gmail.com>
2019-03-01 16:47:51 +0000

    Fix pop up div

--- js/script.js
+++ js/script.js
@@ -7,110 +7,56 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-// Add round result message on a pop-up div
+//User name
+const userName_span = document.getElementById('user-name');
 
+// Scores
+let userScore = 0,
+ 		computerScore = 0;
+const userScore_span = document.getElementById("user-score"),
+ 			computerScore_span = document.getElementById("computer-score");
+
+// Buttons
+const buttonRock = document.getElementById('button-rock'),
+			buttonPaper = document.getElementById('button-paper'),
+			buttonScissors = document.getElementById('button-scissors');
+let buttonName;
+
+// Choices
+let moveId,
+		computerMove,
+		playerMove,
+		randomNumber;
+
+// Round end
+const round = document.getElementById('round'),
+			buttonRound = document.getElementById('button-round');
+let roundResult = document.getElementById('round-result');
+
+// Get user name
+let userName = prompt('hi! what\'s your name?', 'user');
+console.log(userName);
+userName_span.innerHTML = userName;
+
+// Add round result message on a pop-up div
 function printResult(result) {
-	const roundResult = document.getElementById('round');
-	const buttonRound = document.getElementById('button-round');
-	roundResult.style.display = 'block';
-	roundResult.innerHTML += result;
+	round.style.display = 'block';
+	roundResult.innerHTML = result;
 }
 
 // Hide pop-up div with round result and clear score
-
 function hideRoundResult() {
-	document.getElementById('round').style.visibility = 'hidden';
+	round.style.display = 'none';
 	userScore = 0;
 	userScore_span.innerHTML = userScore;
 	computerScore = 0;
 	computerScore_span.innerHTML = computerScore;
 }
 
-// Get user name
-
-let userName = prompt('hi! what\'s your name?', 'user');
-const userName_span = document.getElementById('user-name');
-console.log(userName);
-userName_span.innerHTML = userName;
-
-// Scores
-
-let userScore = 0;
-let computerScore = 0;
-const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score");
-const scoreBoard_div = document.querySelector(".score-board");
-
-// Buttons
-
-const buttonRock = document.getElementById('button-rock');
-const buttonPaper = document.getElementById('button-paper');
-const buttonScissors = document.getElementById('button-scissors');
-let buttonName;
-
-// Main function
-
+// Players choice, computers choice
 function buttonClicked(buttonName) {
   clearMessages();
   console.log(buttonName + ' was clicked');
-
-	let moveId;
-	let computerMove;
-	let playerMove;
-	let randomNumber;
-
-	function getMoveName(moveId) {
-	  console.log('call the function with an argument: ' + moveId);
-	  if (moveId == 1) {
-	    return 'rock';
-	  } else if (moveId == 2) {
-	    return 'paper';
-	  } else if (moveId == 3) {
-	    return 'scissors';
-	  }
-	}
-
-	// Compare choices + result
-
-	function displayResult(playerMove, computerMove) {
-	  console.log('call the function with arguments: ' + playerMove + ', ' + computerMove);
-	  if (playerMove == 'paper' && computerMove == 'rock') {
-	    printMessage('You win!!');
-			userScore++;
-			userScore_span.innerHTML = userScore;
-	  } else if (playerMove == 'rock' && computerMove == 'scissors') {
-	    printMessage('You win!!');
-			userScore++;
-			userScore_span.innerHTML = userScore;
-	  } else if (playerMove == 'scissors' && computerMove == 'paper') {
-	    printMessage('You win ;)!');
-			userScore++;
-			userScore_span.innerHTML = userScore;
-	  } else if (playerMove == computerMove) {
-	    printMessage('It\'s a draw!');
-	  } else {
-	    printMessage('You lose :(');
-			computerScore++;
-			computerScore_span.innerHTML = computerScore;
-	  }
-	  printMessage('I throw ' + computerMove + ', you throw ' + playerMove + '.');
-	}
-
-	// Round result
-
-	function roundEndAlert(userScore, computerScore) {
-		console.log('call the function with arguments: ' + userScore + ',' + computerScore);
-		if (userScore == '3') {
-			console.log(userName + ' won this round');
-			printResult('You WON this round!');
-		} else if (computerScore == '3') {
-			console.log('Computer won this round');
-			printResult('You LOST this round :(');
-		}
-	}
-
-// Computer's choices
-
 	playerMove = buttonName;
 	console.log('players move is : ' + playerMove);
 	randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -121,7 +67,53 @@ function buttonClicked(buttonName) {
 	roundEndAlert(userScore, computerScore);
 }
 
-// Player's choice
+function getMoveName(moveId) {
+  console.log('call the function with an argument: ' + moveId);
+  if (moveId == 1) {
+    return 'rock';
+  } else if (moveId == 2) {
+    return 'paper';
+  } else if (moveId == 3) {
+    return 'scissors';
+  }
+}
+
+// Compare choices
+function userWin(){
+	printMessage('You win!!');
+	userScore++;
+	userScore_span.innerHTML = userScore;
+}
+
+function displayResult(playerMove, computerMove) {
+  console.log('call the function with arguments: ' + playerMove + ', ' + computerMove);
+  if (playerMove == 'paper' && computerMove == 'rock') {
+    userWin(userScore);
+  } else if (playerMove == 'rock' && computerMove == 'scissors') {
+    userWin(userScore);
+  } else if (playerMove == 'scissors' && computerMove == 'paper') {
+    userWin(userScore);
+  } else if (playerMove == computerMove) {
+    printMessage('It\'s a draw!');
+  } else {
+    printMessage('You lose :(');
+		computerScore++;
+		computerScore_span.innerHTML = computerScore;
+  }
+  printMessage('I throw ' + computerMove + ', you throw ' + playerMove + '.');
+}
+
+// Round result
+function roundEndAlert(userScore, computerScore) {
+	console.log('call the function with arguments: ' + userScore + ',' + computerScore);
+	if (userScore == 3) {
+		console.log(userName + ' won this round');
+		printResult('You WON this round!');
+	} else if (computerScore == 3) {
+		console.log('Computer won this round');
+		printResult('You LOST this round :(');
+	}
+}
 
 buttonRock.addEventListener('click', function(){
 	buttonClicked('rock')
@@ -132,3 +124,4 @@ buttonPaper.addEventListener('click', function(){
 buttonScissors.addEventListener('click', function(){
 	buttonClicked('scissors')
 });
+buttonRound.addEventListener('click', hideRoundResult);
